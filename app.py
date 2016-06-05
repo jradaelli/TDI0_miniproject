8552c9c5a5cf7e3cbc0824e4c3c180370b5e243c
jradaelli <j.radaelli@reply.it>
2016-06-05 16:55:08 -0400

    First commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect
+from graph import get_data, create_graph
+from bokeh.embed import components
+
+app = Flask(__name__)
+
+app.vars = {}
+
+
+@app.route('/')
+def main():
+    return redirect('/index')
+
+
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        app.vars["stock_ticker"] = request.form['ticker']
+        app.logger.debug(app.vars["stock_ticker"])
+        app.vars["columns_name"] = request.form.getlist('features')
+        app.logger.debug(app.vars["columns_name"])
+        final_data = get_data(
+            app.vars["stock_ticker"], app.vars["columns_name"])
+        plot = create_graph(final_data, app.vars["columns_name"])
+        script, div = components(plot)
+        app.logger.debug(components(plot))
+        return render_template('graph.html', script=script, div=div)
+
+
+if __name__ == '__main__':
+    app.run(port=33507, debug='debug')
---
+++ graph.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import requests as req
+import numpy as np
+from bokeh.plotting import figure, output_file
+from bokeh.palettes import Spectral11
+
+
+def get_data(stock='ADI', checkbox_list='Open'):
+    r = req.get('https://www.quandl.com/api/v3/datasets/WIKI/' +
+                stock + '.json')
+    raw_data = r.json()
+    final_data = pd.DataFrame(raw_data['dataset']['data'], columns=raw_data[
+                              'dataset']['column_names'])
+    final_data = final_data.set_index('Date')
+    final_data = final_data.ix[:, checkbox_list]
+    return final_data
+
+
+# final_data = get_data()
+
+# checkbox_list = ['Open', 'Close']
+
+def datetime(x):
+    return np.array(x, dtype=np.datetime64)
+
+
+def create_graph(final_data, checkbox_list):
+    p1 = figure(x_axis_type="datetime")
+    p1.title = "Stock Prices"
+    p1.grid.grid_line_alpha = 0.3
+    p1.xaxis.axis_label = 'Date'
+    p1.yaxis.axis_label = 'Price'
+    numlines = len(checkbox_list)
+    mypalette = Spectral11[0:numlines]
+
+    p1.multi_line(xs=[datetime(final_data.index.values)] * numlines,
+                  ys=[final_data[name].values for name in final_data], line_color=mypalette, line_width=1)
+    #output_file("templates/stocks.html", title="Stock Analysis")
+    return p1
