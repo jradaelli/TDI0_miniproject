0770b472b3979498b52dfc41f76a456d4b1773a8
jradaelli <j.radaelli@reply.it>
2016-06-05 22:57:37 -0400

    add utf8 encription

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect
 from graph import get_data, create_graph
 from bokeh.embed import components
+from bokeh.util.string import encode_utf8
+
 
 app = Flask(__name__)
 
@@ -25,8 +27,9 @@ def index():
             app.vars["stock_ticker"], app.vars["columns_name"])
         plot = create_graph(final_data, app.vars["columns_name"])
         script, div = components(plot)
-        return render_template('graph.html', script=script, div=div)
+        html = render_template('graph.html', script=script, div=div)
+        return encode_utf8(html)
 
 
 if __name__ == '__main__':
-    app.run(debug='debug')
+    app.run(debug=True)
